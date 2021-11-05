037f94b1682be6aaa5ed2fbfcc59166c3f43db99
Paul <pauldro952@outlook.com>
2021-11-05 14:09:09 -0500

    Add Item Images

--- site/templates/controllers/classes/mii/Ii/Item.php
+++ site/templates/controllers/classes/mii/Ii/Item.php
@@ -164,7 +164,8 @@ class Item extends IiFunction {
 		$page->refreshurl   = self::itemUrl($data->itemID, true);
 		$item = ItemMasterItemQuery::create()->findOneByItemid($data->itemID);
 		$itempricing = ItemPricingQuery::create()->findOneByItemid($data->itemID);
-		return $config->twig->render('items/ii/item/display.twig', ['item' => $item, 'itempricing' => $itempricing, 'html' => $html]);
+		$docm = Documents::getDocFinderIi();
+		return $config->twig->render('items/ii/item/display.twig', ['item' => $item, 'itempricing' => $itempricing, 'html' => $html, 'docm' => $docm]);
 	}
 
 	protected static function displaySectionFormatted($data, $jsonInfo) {
